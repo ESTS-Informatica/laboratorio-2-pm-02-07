7e1e70145aa220e2e2cb1d0414ec4fe26caa9268
diogorvalente <202100732@estudantes.ips.pt>
2024-03-20 11:43:58 +0000

    nivel1 feito

--- Property.java
+++ Property.java
@@ -1,5 +1,3 @@
-
-
 /**
  * A property to sell.
  *
@@ -9,7 +7,7 @@
 public class Property {
 
     private static int numberOfProperties = 0;
-    private final String id;
+    private final int id;
     private String description;
     private double price;
 
@@ -20,21 +18,24 @@ public class Property {
      * @param price The property price.
      */
     public Property(String description, double price) {
-
+        this.id = ++numberOfProperties;
+        this.description = (description != null)? description : "";
+        this.price = (price > 0) ? price : 0.0;
     }
 
     /**
      * Id selector.
      */
     public int getId() {
-        
+
+        return this.id;
     }
 
     /**
      * Description selector.
      */
     public String getDescription() {
-
+        return this.description;
     }
 
     /**
@@ -43,14 +44,17 @@ public class Property {
      * @param description The new description. Must not be null.
      */
     public void setDescription(String description) {
-
+        if (description != null) {
+            this.description = description;
+        }
+        
     }
 
     /**
      * Price selector.
      */
     public double getPrice() {
-        
+        return this.price;
     }
     
     /**
@@ -59,10 +63,13 @@ public class Property {
      * @param price The new price. Must not be negative.
      */
     public void setPrice(double price) {
-
+        if (price > 0) {
+            this.price = price;
+        }
     }
 
     @Override
     public String toString() {
-    }
+        return this.numberOfProperties + "\t(" + this.description + ")\t" + this.price;
+}
 }
